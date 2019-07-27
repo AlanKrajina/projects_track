f2a917196af768211143ef626509529eeb9deb64
Alan Krajina <alankrajina88@gmail.com>
2019-07-27 15:19:57 +0000

    add seed

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+alan = User.create(username: "Alan", email: "alan@gmail.com", password: "password")
+john = User.create(username: "John", email: "john@gmail.com", password: "password")
+
+Project.create(name: "Sinatra App", content: "text text text text text text text ", completion: "2019", user_id: alan.id)
+Project.create(name: "Rails App", content: "text2 text2 text2 text2 text2 text2 text2 ", completion: "2020", user_id: john.id)
+
+a = Score.create(grade: "A")
+b = Score.create(grade: "B")
+c = Score.create(grade: "C")
+d = Score.create(grade: "D")
+f = Score.create(grade: "F")

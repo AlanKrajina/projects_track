2f782af8d43d78f04a9f59923656abdb4233b537
Alan Krajina <alankrajina88@gmail.com>
2019-07-28 11:12:43 +0000

    add projects erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,86 @@
 class ProjectsController < ApplicationController
+  
+  get '/projects' do
+    if logged_in?
+      @projects = Project.all
+      erb :'projects/projects'
+    else
+      redirect to '/login'
+    end
+  end  
+  
+  
+  get '/projects/new' do
+    if logged_in?
+      erb :'projects/create_project'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  post '/projects' do
+    if logged_in?
+      if params[:name] == "" || params[:content] == "" || params[:completion] == ""
+        redirect to "/projects/new"
+      else
+        @project = current_user.projects.build(name: params[:name], content: params[:content], completion: params[:completion])
+        if @project.save
+          redirect to "/projects/#{@project.id}"
+        else
+          redirect to "/projects/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
+  
+  get '/projects/:id' do
+    if logged_in?
+      @project = Project.find_by_id(params[:id])
+      erb :'projects/show_project'
+    else
+      redirect to '/login'
+    end
+  end
+  
+ 
+ 
+ 
+ 
+  get '/projects/:id/edit' do
+    if logged_in?
+      @project = Project.find_by_id(params[:id])
+      if @project && @project.user == current_user
+        erb :'projects/edit_project'
+      else
+        redirect to '/projects'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/projects/:id' do
+    if logged_in?
+      if params[:name] == "" || params[:content] == "" || params[:completion] == ""
+        redirect to "/projects/#{params[:id]}/edit"
+      else
+        @project = Project.find_by_id(params[:id])
+        if @project && @project.user == current_user
+          if @project.update(name: params[:name], content: params[:content], completion: params[:completion])
+            redirect to "/projects/#{@project.id}"
+          else
+            redirect to "/projects/#{@project.id}/edit"
+          end
+        else
+          redirect to '/projects'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end 
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,47 @@ class UsersController < ApplicationController
       end
   end
   
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/projects'
+    end
+  end 
+  
+  
+  
+  
+   get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/projects'
+    end
+  end 
+  
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/projects"
+    else
+      redirect to '/signup'
+    end
+  end  
+  
+  
+  
+  
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end  
 end

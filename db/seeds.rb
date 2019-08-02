0bb346aae1f98578e1c2fffed0defb602a5cf050
Alan Krajina <alankrajina88@gmail.com>
2019-08-02 12:15:05 +0000

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-alan = User.create(username: "Alan", email: "alan@gmail.com", password: "password")
-john = User.create(username: "John", email: "john@gmail.com", password: "password")
-
-sinatra = Project.create(name: "Sinatra App", content: "text text text text text text text ", completion: "2019", user_id: alan.id)
-rails = Project.create(name: "Rails App", content: "text2 text2 text2 text2 text2 text2 text2 ", completion: "2020", user_id: john.id)
-
 Result.create(info: "Attempt 1")
 Result.create(info: "Attempt 2")
 Result.create(info: "Attempt 3")
